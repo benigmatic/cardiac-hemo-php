d46bd4fe4a567517d4c3a1c85afa41b7aa6e143c
Alexandra <70664290+benigmatic@users.noreply.github.com>
2023-01-18 14:34:27 -0500

    Updated UI

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ echo "testing login info";
  if(!mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL)){
      die('Failed to connect to MySQL: '.mysqli_connect_error());
  }
- echo "connection established";
+ echo "connection established <br>";
  //Test if table exists
  $res = mysqli_query($conn, "SHOW TABLES LIKE 'students'");
 
@@ -23,9 +23,9 @@ echo "testing login info";
      else {
          echo "Data shhown";
          while ($row = mysqli_fetch_assoc($res)) {
-             echo "1. ".$row["SID"]." </td>";
-             echo "2. ".$row["Lastname"]." </td> </tr>";
-             echo "3. ".$row["FirstName"]." </td> </tr>";
+             echo "SID. ".$row["SID"]." <br>";
+             echo "Lastname:  ".$row["Lastname"]." <br>";
+             echo "FirstName:  ".$row["FirstName"]." <br>";
          }
          echo "</table>";
      }
