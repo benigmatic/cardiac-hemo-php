d31c275e3378a911cbaf4c0340095003cc0d489f
Alexandra <70664290+benigmatic@users.noreply.github.com>
2023-01-18 14:32:07 -0500

    Checks if students table exists and returns SID

--- index.php
+++ index.php
@@ -8,4 +8,25 @@ echo "testing login info";
      die('Failed to connect to MySQL: '.mysqli_connect_error());
  }
  echo "connection established";
+ //Test if table exists
+ $res = mysqli_query($conn, "SHOW TABLES LIKE 'students'");
 
+ if (mysqli_num_rows($res) <= 0) {
+     echo "/n No students";
+ } else {
+     //Query and print data
+     $res = mysqli_query($conn, 'SELECT * FROM Products');
+
+     if (mysqli_num_rows($res) <= 0) {
+         echo "No Students found in the table";
+     }
+     else {
+         echo "Data shhown";
+         while ($row = mysqli_fetch_assoc($res)) {
+             echo "1. ".$row["SID"]." </td>";
+             echo "2. ".$row["Lastname"]." </td> </tr>";
+             echo "3. ".$row["FirstName"]." </td> </tr>";
+         }
+         echo "</table>";
+     }
+ }
