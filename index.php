04491f07cb7be4ffdba99b9861e8fbc2b5300c00
Alexandra <70664290+benigmatic@users.noreply.github.com>
2023-01-18 14:27:10 -0500

    Database connection

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
+require "database/config.php";
 echo "testing login info";
+ //Establish the connection
+ $conn = mysqli_init();
+ mysqli_ssl_set($conn,NULL,NULL,$sslcert,NULL,NULL);
+ if(!mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL)){
+     die('Failed to connect to MySQL: '.mysqli_connect_error());
+ }
+ echo "connection established";
+
