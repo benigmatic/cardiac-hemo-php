1d6ac3c7433546486ea9b13b32a263e785656302
hpipyT <69983604+hpipyT@users.noreply.github.com>
2023-01-25 15:50:34 -0500

    Update send-attempt.php

--- send-attempt.php
+++ send-attempt.php
@@ -2,27 +2,17 @@
 
 require "database/config.php";
 
-try
-{
-	$dbh = mysqli_init();
-    mysqli_ssl_set($conn,NULL,NULL,$sslcert,NULL,NULL);
-    if(!mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL)){
-        die('Failed to connect to MySQL: '.mysqli_connect_error());
-    } 
-    SendAttempt($_POST["SID"], $_POST["FirstName"], $_POST["ClassSection"]);
-}
-catch(Exception $e)
-{
-	echo '<h1>An error has occurred.</h1><pre>', $e->getMessage()
-            ,'</pre>';
-}
+$dbh = mysqli_init();
+mysqli_ssl_set($conn,NULL,NULL,$sslcert,NULL,NULL);
+if(!mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL))
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
 
-function SendAttempt($LastName, $FirstName, $ClassSection)
-{
-	GLOBAL $dbh;
-	$sql = "INSERT INTO Students (LastName,FirstName,ClassSection) VALUES(?,?,?)";
-	$sth = $dbh->prepare($sql);
-	$sth->execute(array($LastName, $FirstName, $ClassSection));
-	exit();
-}
+$SID = $_POST["SID"];
+$FirstName = $_POST["FirstName"];
+$ClassSection = $_POST["ClassSection"];
+
+$sql = "INSERT INTO Students (LastName,FirstName,ClassSection) VALUES(?,?,?)";
+$sth = $dbh->prepare($sql);
+$sth->execute(array($LastName, $FirstName, $ClassSection));
+exit();
 ?>
