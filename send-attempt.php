3391c6e90e36a93df427f119024e46e0e008c199
hpipyT <69983604+hpipyT@users.noreply.github.com>
2023-01-26 04:49:47 -0500

    Update send-attempt.php

--- send-attempt.php
+++ send-attempt.php
@@ -1,18 +1,45 @@
 <?php
+/* What it does:
+    Receives the login information from Unity,
+    runs query to find the user with the same login SID, validates them 
+    returns 0 if the user doesn't exist, returns json with Section number (App Settings if needed) 
+    URL: https://hemo-cardiac.azurewebsites.net//login.php?var1=SID_value 
+        where SID_value is the SID sent from Unity
+    Source for PHP for variable parsing: https://stackoverflow.com/questions/44759249/unity-c-sharp-send-variable-to-php-server
+*/
 
 require "database/config.php";
-
-$dbh = mysqli_init();
+//Establish the connection
+$conn = mysqli_init();
 mysqli_ssl_set($conn,NULL,NULL,$sslcert,NULL,NULL);
-if(!mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL))
+if(!mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL)){
     die('Failed to connect to MySQL: '.mysqli_connect_error());
+} 
 
-// $SID = $_POST["SID"];
-// $FirstName = $_POST["FirstName"];
-// $ClassSection = $_POST["ClassSection"];
+// Get login SID from Unity, if there is no SID in the URL, the sript shows 400 error
+if (isset($_REQUEST["var1"])) {
 
-// $sql = "INSERT INTO Students (LastName,FirstName,ClassSection) VALUES(?,?,?)";
-// $sth = $dbh->prepare($sql);
-// $sth->execute(array($LastName, $FirstName, $ClassSection));
-
+    //TODO: Test this: Prevents Injection
+    /*
+    $SID = mysql_real_escape_string($_REQUEST["var1"], $conn);
+    */
+    $SID = &$_REQUEST["var1"];
+    // Run query to select a student from the database
+    $query = "SELECT FirstName, ClassSection FROM students WHERE SID='$SID'";
+    $res = mysqli_query($conn, $query); 
+    if (mysqli_num_rows($res) <= 0) {
+        echo "No Students found in the table";
+    }
+    else {
+        $row = mysqli_fetch_assoc($res);
+        $Name = &$row["FirstName"];
+        $Section = &$row["ClassSection"];
+        //Creates a json file to return
+        $Login_res = new stdClass();
+        $Login_res->objects = [$SID, $Name,$Section ];
+        echo json_encode($Login_res);
+    }
+} else {
+    http_status_code(400);
+}
 ?>
