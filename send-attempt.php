9110859eb6dd6c4e8140d2e1ebf58b9bd565edc1
hpipyT <69983604+hpipyT@users.noreply.github.com>
2023-01-26 03:22:34 -0500

    Update send-attempt.php

--- send-attempt.php
+++ send-attempt.php
@@ -7,12 +7,12 @@ mysqli_ssl_set($conn,NULL,NULL,$sslcert,NULL,NULL);
 if(!mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL))
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 
-$SID = $_POST["SID"];
-$FirstName = $_POST["FirstName"];
-$ClassSection = $_POST["ClassSection"];
+// $SID = $_POST["SID"];
+// $FirstName = $_POST["FirstName"];
+// $ClassSection = $_POST["ClassSection"];
 
-$sql = "INSERT INTO Students (LastName,FirstName,ClassSection) VALUES(?,?,?)";
-$sth = $dbh->prepare($sql);
-$sth->execute(array($LastName, $FirstName, $ClassSection));
-exit();
+// $sql = "INSERT INTO Students (LastName,FirstName,ClassSection) VALUES(?,?,?)";
+// $sth = $dbh->prepare($sql);
+// $sth->execute(array($LastName, $FirstName, $ClassSection));
+
 ?>
