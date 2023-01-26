0b6755c20c10263742cc885e68106db3b8bd30d1
hpipyT <69983604+hpipyT@users.noreply.github.com>
2023-01-26 07:33:58 -0500

    Create hemo-attempt.php

---
+++ hemo-attempt.php
@@ -0,0 +1,42 @@
+<?php
+
+// Adds a DrHemo_attempt to the database
+
+// Database authorization
+require "database/config.php";
+
+// Establish the connection
+$conn = mysqli_init();
+mysqli_ssl_set($conn,NULL,NULL,$sslcert,NULL,NULL);
+if(!mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+} 
+
+// Assign table input from POST request
+// real_escape_string sanitizes input to prevent SQL injection 
+$aid = intval($conn->real_escape_string($_POST['AID']));
+$name = $conn->real_escape_string($_POST['Name']);
+$time = intval($conn->real_escape_string($_POST['Time']));
+$completed = intval($conn->real_escape_string($_POST['Completed']));
+$sid1 = intval($conn->real_escape_string($_POST['SID1']));
+$sid2 = intval($conn->real_escape_string($_POST['SID2']));
+$sid3 = intval($conn->real_escape_string($_POST['SID3']));
+$sid4 = intval($conn->real_escape_string($_POST['SID4']));
+$sid5 = intval($conn->real_escape_string($_POST['SID5']));
+
+// Prepared statement ensures matching data types
+$stmt = $conn->prepare("INSERT INTO DrHemo_attempts (AID, Name, Time, Completed, SID1, SID2, SID3, SID4, SID5) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+$stmt->bind_param("isiiiiiii", $aid, $name, $time, $completed, $sid1, $sid2, $sid3, $sid4, $sid5);
+
+// return statements
+if ($stmt->execute())
+{
+    echo "New record created successfully";
+} 
+else 
+{
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+?>
