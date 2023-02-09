6f6af7199682286c8752dc7b93ba927bccce71ef
hpipyT <69983604+hpipyT@users.noreply.github.com>
2023-02-09 18:27:47 -0500

    Update create-hemo-attempt.php

--- create-hemo-attempt.php
+++ create-hemo-attempt.php
@@ -16,8 +16,8 @@ if(!mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQ
 // Assign table input from POST request
 // real_escape_string sanitizes input to prevent SQL injection 
 $name = $conn->real_escape_string($_POST['Name']);
-$time = floatval($conn->real_escape_string($_POST['TimeSpent']));
-$completed = intval($conn->real_escape_string($_POST['Completed']));
+// $time = floatval($conn->real_escape_string($_POST['TimeSpent']));
+// $completed = intval($conn->real_escape_string($_POST['Completed']));
 $sid1 = intval($conn->real_escape_string($_POST['SID1']));
 
 $sid2 = intval($conn->real_escape_string($_POST['SID2']));
@@ -26,9 +26,10 @@ $sid4 = intval($conn->real_escape_string($_POST['SID4']));
 $sid5 = intval($conn->real_escape_string($_POST['SID5']));
 
 // Prepared statement ensures matching data types
-$stmt = $conn->prepare("INSERT INTO drhemo_attempts (Name, Time, Completed, SID1, SID2, SID3, SID4, SID5) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+$stmt = $conn->prepare("INSERT INTO drhemo_attempts (Name, SID1, SID2, SID3, SID4, SID5) VALUES (?, ?, ?, ?, ?, ?)");
+// $stmt = $conn->prepare("INSERT INTO drhemo_attempts (Name, Time, Completed, SID1, SID2, SID3, SID4, SID5) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
 echo "Before statment";
-$stmt->bind_param("sdiiiiii", $name, $time, $completed, $sid1, $sid2, $sid3, $sid4, $sid5);
+$stmt->bind_param("siiiii", $name, $time, $completed, $sid1, $sid2, $sid3, $sid4, $sid5);
 echo "After statement";
 
 // return statements
