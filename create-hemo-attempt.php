3247b5459e1d675b60fc69d8a7cc753815a82882
hpipyT <69983604+hpipyT@users.noreply.github.com>
2023-02-09 18:07:36 -0500

    Update create-hemo-attempt.php

--- create-hemo-attempt.php
+++ create-hemo-attempt.php
@@ -13,20 +13,28 @@ if(!mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQ
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 } 
 
+echo "checkmark1";
 // Assign table input from POST request
 // real_escape_string sanitizes input to prevent SQL injection 
 $name = $conn->real_escape_string($_POST['Name']);
+echo "checkmark5";
 $time = floatval($conn->real_escape_string($_POST['TimeSpent']));
+echo "checkmark6";
 $completed = intval($conn->real_escape_string($_POST['Completed']));
+echo "checkmark7";
 $sid1 = intval($conn->real_escape_string($_POST['SID1']));
+
 $sid2 = intval($conn->real_escape_string($_POST['SID2']));
 $sid3 = intval($conn->real_escape_string($_POST['SID3']));
 $sid4 = intval($conn->real_escape_string($_POST['SID4']));
 $sid5 = intval($conn->real_escape_string($_POST['SID5']));
 
+echo "checkmark8";
 // Prepared statement ensures matching data types
 $stmt = $conn->prepare("INSERT INTO drhemo_attempts (Name, Time, Completed, SID1, SID2, SID3, SID4, SID5) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+echo "checkmark9";
 $stmt->bind_param("siiiiiii", $name, $time, $completed, $sid1, $sid2, $sid3, $sid4, $sid5);
+echo "checkmark10";
 
 // return statements
 if ($stmt->execute())
