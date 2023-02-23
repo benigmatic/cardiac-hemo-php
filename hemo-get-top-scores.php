a9381eb3ebbcd24e0c16827fa6e00678c291a858
hpipyT <69983604+hpipyT@users.noreply.github.com>
2023-02-23 12:04:09 -0500

    Create hemo-get-top-scores.php

---
+++ hemo-get-top-scores.php
@@ -0,0 +1,35 @@
+<?php
+
+// retrieves top 3 attempts ordered by lowest TimeSpent
+
+// Receives: nothing
+// Sends: JSON object
+
+require "database/config.php";
+
+$conn = mysqli_init();
+mysqli_ssl_set($conn,NULL,NULL,$sslcert,NULL,NULL);
+if(!mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+} 
+
+$sth = $conn->prepare('SELECT Name, TimeSpent, SID1, SID2, SID3, SID4, SID5 FROM drhemo_attempts ORDER BY TimeSpent ASC LIMIT 3');
+$sth->execute();
+
+$result = $sth->get_result();
+
+if ($result->num_rows > 0)
+{
+    header('Content-Type: application/json');
+    $array = array();
+    while ($row = $result->fetch_assoc())
+    {
+        $array[] = $row;
+        // I will assume for now that this captures class variables like a Hemo Attempt
+    }
+
+    echo json_encode($array);
+}
+
+?>
