fab4cae4f67e45a54af30fe625dc2c7fa804bd41
hpipyT <69983604+hpipyT@users.noreply.github.com>
2023-03-23 04:15:29 -0400

    Update hemo-update-attempt.php

--- hemo-update-attempt.php
+++ hemo-update-attempt.php
@@ -9,22 +9,23 @@ if(!mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQ
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 } 
-echo "check1";
+
+# typical update stuffs
 $aid = intval($conn->real_escape_string($_POST['GAMEid']));
 $time = floatval($conn->real_escape_string($_POST['TimeSpent']));
 $completed = intval((isset($_POST['Completed']) && !empty($_POST['Completed'])) ? $conn->real_escape_string($_POST['Completed']) : 0);
-echo "check2";
+
+# new joiner stuff
 $sid = intval($conn->real_escape_string($_POST['SID']));
 $num = intval($conn->real_escape_string($_POST['JoinNum']));
-echo "check3";
-//$joiner = 'SID' . $num;
-$joiner = 'SID' . "2";
-echo "check4";
+#echo "check3";
+$joiner = 'SID' . $num;
+#echo "check4";
+
+# prepared statement
 $stmt = $conn->prepare("UPDATE drhemo_attempts SET TimeSpent = ?, Completed = ?, $joiner = ? WHERE GAMEid = ?");
-echo "check5";
-echo $joiner;
+#echo $joiner;
 $stmt->bind_param("diii", $time, $completed, $sid, $aid);
-echo "check6";
 // return statements
 if ($stmt->execute())
 {
