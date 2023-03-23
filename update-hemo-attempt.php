933da233c16f40381086b73d70af0aa797392b34
hpipyT <69983604+hpipyT@users.noreply.github.com>
2023-03-23 01:47:37 -0400

    Update update-hemo-attempt.php

--- update-hemo-attempt.php
+++ update-hemo-attempt.php
@@ -10,12 +10,12 @@ if(!mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQ
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 } 
 
-$aid = intval($conn->real_escape_string($_POST['AID']));
+$aid = intval($conn->real_escape_string($_POST['GAMEid']));
 $time = floatval($conn->real_escape_string($_POST['TimeSpent']));
 $completed = (isset($_POST['Completed']) && !empty($_POST['Completed'])) ? $conn->real_escape_string($_POST['Completed']) : 0;
 
 
-$stmt = $conn->prepare("UPDATE drhemo_attempts SET TimeSpent = ?, Completed = ? WHERE AID = ?");
+$stmt = $conn->prepare("UPDATE drhemo_attempts SET TimeSpent = ?, Completed = ? WHERE GAMEid = ?");
 $stmt->bind_param("dii", $time, $completed, $aid);
 
 // return statements
