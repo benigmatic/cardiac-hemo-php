a7b0de22f7715fb03679c40781c90863df30d600
hpipyT <69983604+hpipyT@users.noreply.github.com>
2023-03-23 07:23:47 -0400

    Update hemo-create-puzzleStep2.php

--- hemo-create-puzzleStep2.php
+++ hemo-create-puzzleStep2.php
@@ -12,7 +12,7 @@ if(!mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQ
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 } 
-
+echo "check1";
 // JSON from Unity
 $aid = intval($conn->real_escape_string($_POST['GAMEid']));
 $puzzlename = $conn->real_escape_string($_POST['PuzzleName']);
@@ -21,29 +21,33 @@ $hintstaken = intval($conn->real_escape_string($_POST['HintsTaken']));
 $time = floatval($conn->real_escape_string($_POST['TimeTaken']));
 
 // check if these already have an entry in the table
-
+echo "check2";
 // Prepare the statement
 $stmt = $conn->prepare("SELECT PUZSTEPid FROM drhemo_puzzlesteps WHERE GAMEid = ? AND PuzzleName = ? AND PuzzleStep = ?");
 $stmt->bind_param("isi", $aid, $puzzlename, $puzzlestep);
-
+echo "check3";
 // Execute the statement
 $stmt->execute();
 $result = $stmt->get_result();
-
+echo "check4";
 // Check if the PuzzleStep already exists
 if ($result->num_rows == 0)
 {
   // If not, create a new PuzzleStep
+    echo "check5";
   $stmt = $conn->prepare("INSERT INTO drhemo_puzzlesteps (GAMEid, PuzzleName, PuzzleStep, HintsTaken, TimeTaken) VALUES (?, ?, ?, ?, ?)");
   $stmt->bind_param("isiid", $aid, $puzzlename, $puzzlestep, $hintstaken, $time);
+    echo "check6";
   if($stmt->execute())
   {
       // returning PUZid
+      echo "check7";
       $ret = mysqli_insert_id($conn);
       echo $ret;
   }
   else
   {
+      echo "check8";
       echo "Error: " . $stmt->err . "<br>" . $conn->error;
   }
 
@@ -51,6 +55,7 @@ if ($result->num_rows == 0)
 else 
 {
     // return the PUZid
+    echo "check9";
     echo $result;
 }
 ?>
