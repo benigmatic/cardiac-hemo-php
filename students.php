c2646701d6cb7f6d52f1c2d9190398b1b2df829a
Alexandra <70664290+benigmatic@users.noreply.github.com>
2023-03-29 19:40:35 -0400

    Removed the full list of students data from default screen index.php

---
+++ students.php
@@ -0,0 +1,29 @@
+<?php
+// Creates a connection with the database, runs queries: tests that table exists and return all values from students.
+require "database/config.php";
+ //Establish the connection
+ $conn = mysqli_init();
+ mysqli_ssl_set($conn,NULL,NULL,$sslcert,NULL,NULL);
+ if(!mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL)){
+     die('Failed to connect to MySQL: '.mysqli_connect_error());
+ }
+ //Test if table exists
+ $res = mysqli_query($conn, "SHOW TABLES LIKE 'students'");
+
+ if (mysqli_num_rows($res) <= 0) {
+     echo "No students in the table";
+ } else {
+// Selects all data from students table (SID, password, and First Name)
+     $res = mysqli_query($conn, 'SELECT * FROM students');
+
+     if (mysqli_num_rows($res) <= 0) {
+         echo "No Students found in the table";
+     }
+     else {
+         while ($row = mysqli_fetch_assoc($res)) {
+             // echo $row["SID"] . "," . $row["Password"] . "," . $row["FirstName"]."*" . "<br>"; // feel free to revert
+             echo "SID: " . $row["SID"] . ", ClassSection:" . $row["ClassSection"] . ", Name:" . $row["FirstName"] . "<br>"; // plz comment insted of delet ty
+         }
+         
+     }
+ }
