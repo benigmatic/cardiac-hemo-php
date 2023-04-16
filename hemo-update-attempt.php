7e8066396233a003958e6970dbea0441ce663cb1
hpipyT <69983604+hpipyT@users.noreply.github.com>
2023-04-16 18:46:25 -0400

    Update hemo-update-attempt.php

--- hemo-update-attempt.php
+++ hemo-update-attempt.php
@@ -31,8 +31,9 @@ echo "updateDebug: 3";
 try{
     echo "updateDebug: 4";
     $stmt = $conn->prepare("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'drhemo_attempts'");
-    
+    echo "updateDebug: 4.1";
     $stmt = $conn->prepare("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'drhemo_attempts' AND (SID1 = 0 OR SID2 = 0 OR SID3 = 0 OR SID4 = 0 OR SID5 = 0) LIMIT 1");
+    echo "updateDebug: 4.2";
     $stmt->execute();
     $stmt->bind_result($column_name);
     $stmt->fetch();
