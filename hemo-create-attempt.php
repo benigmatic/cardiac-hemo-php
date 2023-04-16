dc9e3e14fa1b9ebe17b58e91a31c6c3532c43634
hpipyT <69983604+hpipyT@users.noreply.github.com>
2023-04-16 16:33:00 -0400

    Update hemo-create-attempt.php

--- hemo-create-attempt.php
+++ hemo-create-attempt.php
@@ -19,16 +19,16 @@ $name = $conn->real_escape_string($_POST['TeamName']);
 $time = floatval($conn->real_escape_string($_POST['TimeSpent']));
 // $completed = intval($conn->real_escape_string($_POST['Completed']));
 $completed = (isset($_POST['Completed']) && !empty($_POST['Completed'])) ? $conn->real_escape_string($_POST['Completed']) : 0;
-$sid1 = intval($conn->real_escape_string($_POST['SID1']));
-$sid2 = intval($conn->real_escape_string($_POST['SID2']));
-$sid3 = intval($conn->real_escape_string($_POST['SID3']));
-$sid4 = intval($conn->real_escape_string($_POST['SID4']));
-$sid5 = intval($conn->real_escape_string($_POST['SID5']));
+$sid1 = $conn->real_escape_string($_POST['SID1']);
+$sid2 = $conn->real_escape_string($_POST['SID2']);
+$sid3 = $conn->real_escape_string($_POST['SID3']);
+$sid4 = $conn->real_escape_string($_POST['SID4']);
+$sid5 = $conn->real_escape_string($_POST['SID5']);
 
 // Prepared statement ensures matching data types
 $stmt = $conn->prepare("INSERT INTO drhemo_attempts (TeamName, TimeSpent, Completed, SID1, SID2, SID3, SID4, SID5) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
 
-$stmt->bind_param("sdiiiiii", $name, $time, $completed, $sid1, $sid2, $sid3, $sid4, $sid5);
+$stmt->bind_param("sdisssss", $name, $time, $completed, $sid1, $sid2, $sid3, $sid4, $sid5);
 
 
 // return statements
