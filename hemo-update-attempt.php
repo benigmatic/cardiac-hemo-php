b9f29cef2cc9da5925d020d79b46c575d7b2f318
hpipyT <69983604+hpipyT@users.noreply.github.com>
2023-04-16 19:37:22 -0400

    Update hemo-update-attempt.php

--- hemo-update-attempt.php
+++ hemo-update-attempt.php
@@ -21,7 +21,7 @@ $sid = intval($conn->real_escape_string($_POST['SID']));
 echo "updateDebug: 3";
 try{
     echo "updateDebug: 4";
-    $stmt = $conn->prepare("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'drhemo_attempts' AND COLUMN_NAME LIKE 'SID%'");
+    $stmt = $conn->prepare("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'drhemo_attempts'");
     echo "updateDebug: 4.1";
     #$stmt = $conn->prepare("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'drhemo_attempts' AND (SID1 = 0 OR SID2 = 0 OR SID3 = 0 OR SID4 = 0 OR SID5 = 0) LIMIT 1");
     echo "updateDebug: 4.2";
