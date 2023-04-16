a5dfee17eb5376197a34e29a88c76b127053b2b2
hpipyT <69983604+hpipyT@users.noreply.github.com>
2023-04-16 17:09:31 -0400

    Update hemo-create-attempt.php

--- hemo-create-attempt.php
+++ hemo-create-attempt.php
@@ -13,23 +13,28 @@ if(!mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQ
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 } 
 
+echo "1";
 // Assign table input from POST request
 // real_escape_string sanitizes input to prevent SQL injection 
 $name = $conn->real_escape_string($_POST['TeamName']);
 $time = floatval($conn->real_escape_string($_POST['TimeSpent']));
+echo "2";
 // $completed = intval($conn->real_escape_string($_POST['Completed']));
 $completed = (isset($_POST['Completed']) && !empty($_POST['Completed'])) ? $conn->real_escape_string($_POST['Completed']) : 0;
+echo "3";
 $sid1 = $conn->real_escape_string($_POST['SID1']);
 $sid2 = $conn->real_escape_string($_POST['SID2']);
 $sid3 = $conn->real_escape_string($_POST['SID3']);
 $sid4 = $conn->real_escape_string($_POST['SID4']);
 $sid5 = $conn->real_escape_string($_POST['SID5']);
+echo "4";
 
 // Prepared statement ensures matching data types
 $stmt = $conn->prepare("INSERT INTO drhemo_attempts (TeamName, TimeSpent, Completed, SID1, SID2, SID3, SID4, SID5) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
 
+echo "5";
 $stmt->bind_param("sdisssss", $name, $time, $completed, $sid1, $sid2, $sid3, $sid4, $sid5);
-
+echo "6";
 
 // return statements
 if ($stmt->execute())
