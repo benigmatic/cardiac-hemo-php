c56676f697b97c467714907178a4a0836e8e8f35
hpipyT <69983604+hpipyT@users.noreply.github.com>
2023-04-16 16:46:12 -0400

    Update hemo-update-attempt.php

--- hemo-update-attempt.php
+++ hemo-update-attempt.php
@@ -16,14 +16,37 @@ $time = floatval($conn->real_escape_string($_POST['TimeSpent']));
 $completed = intval((isset($_POST['Completed']) && !empty($_POST['Completed'])) ? $conn->real_escape_string($_POST['Completed']) : 0);
 
 # new joiner stuff
-$sid = intval($conn->real_escape_string($_POST['SID']));
-$num = intval($conn->real_escape_string($_POST['JoinNum']));
+# $sid = intval($conn->real_escape_string($_POST['SID']));
+# $num = intval($conn->real_escape_string($_POST['JoinNum']));
+# $joiner = 'SID' . $num;
 
-$joiner = 'SID' . $num;
 # prepared statement
-$stmt = $conn->prepare("UPDATE drhemo_attempts SET TimeSpent = ?, Completed = ?, $joiner = ? WHERE GAMEid = ?");
-# will not go past this line when testing in browser, default value of $joiner goes to SID0 which is not in the table
-# assert by setting $num to 2
+# $stmt = $conn->prepare("UPDATE drhemo_attempts SET TimeSpent = ?, Completed = ?, $joiner = ? WHERE GAMEid = ?");
+
+$stmt = $dbh->prepare('SELECT 
+        IFNULL(NULLIF(SID1, \'\'), \'SID1\') AS empty_column,
+        IFNULL(NULLIF(SID2, \'\'), \'SID2\') AS empty_column,
+        IFNULL(NULLIF(SID3, \'\'), \'SID3\') AS empty_column,
+        IFNULL(NULLIF(SID4, \'\'), \'SID4\') AS empty_column,
+        IFNULL(NULLIF(SID5, \'\'), \'SID5\') AS empty_column
+    FROM :table_name
+    WHERE \'\' IN (:sid1, :sid2, :sid3, :sid4, :sid5)
+    LIMIT 1');
+
+$table_name = 'drhemo_attempts';
+$sid1 = 'SID1';
+$sid2 = 'SID2';
+$sid3 = 'SID3';
+$sid4 = 'SID4';
+$sid5 = 'SID5';
+
+$stmt->bindParam(':table_name', $table_name);
+$stmt->bindParam(':sid1', $sid1);
+$stmt->bindParam(':sid2', $sid2);
+$stmt->bindParam(':sid3', $sid3);
+$stmt->bindParam(':sid4', $sid4);
+$stmt->bindParam(':sid5', $sid5);
+
 $stmt->bind_param("diii", $time, $completed, $sid, $aid);
 // return statements
 if ($stmt->execute())
