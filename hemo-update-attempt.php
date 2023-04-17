227dbbd808cb6d40c8c2e8262d0ce29c790a00dd
hpipyT <69983604+hpipyT@users.noreply.github.com>
2023-04-16 22:08:24 -0400

    Update hemo-update-attempt.php

--- hemo-update-attempt.php
+++ hemo-update-attempt.php
@@ -10,16 +10,32 @@ if(!mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQ
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 } 
 
-echo "updateDebug: 3.0";
 # typical update stuffs
 $aid = intval($conn->real_escape_string($_POST['GAMEid']));
 $time = floatval($conn->real_escape_string($_POST['TimeSpent']));
 $completed = intval((isset($_POST['Completed']) && !empty($_POST['Completed'])) ? $conn->real_escape_string($_POST['Completed']) : 0);
+echo "updateDebug: 1 ";
+echo "updateDebug: 2 ";
 # new joiner stuff
 $sid = intval($conn->real_escape_string($_POST['SID']));
 
+$columns = array("SID1", "SID2", "SID3", "SID4", "SID5");
+echo "updateDebug: 3 ";
+echo "updateDebug: 4 ";
+// $stmt = $conn->prepare("SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = N'drhemo_attempts' AND COLUMN_NAME LIKE 'SID%' ");
+foreach ($columns as $column) {
+  // Check if value is 0
+  $query = "SELECT $column FROM table_name WHERE $column = 0 LIMIT 1";
+  $result = mysqli_query($conn, $query);
 
-$stmt = $conn->prepare("SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = N'drhemo_attempts' AND COLUMN_NAME LIKE 'SID%' ");
+  // If value is 0, update to 1 and break loop
+  if (mysqli_num_rows($result) > 0) {
+     echo "Found empty space";
+    $update_query = "UPDATE table_name SET $column = $sid WHERE $column = 0";
+    mysqli_query($conn, $update_query);
+    break;
+  }
+}
 
 $stmt->execute();
 $result = $stmt->get_result();
@@ -27,50 +43,24 @@ $result = $stmt->get_result();
 // $stmt->bind_result($column_name);
 // $stmt->fetch();
 
-echo "updateDebug: 3.9";
-$columns = array();
-echo "Found rows: ";
 while ($row = $result->fetch_assoc()) {
-    echo $row['COLUMN_NAME'] . " ";
-    $columns[] = $row['COLUMN_NAME'];
+    echo $row['COLUMN_NAME'] . "<br>";
 }
-
-echo "updateDebug: 4.0 ";
-foreach ($columns as $column)
-{
-    $stmt = $conn->prepare("SELECT VALUE FROM drhemo_attempts WHERE SID = ?");
-    $stmt->bind_param("s", $column);
-    
-echo "updateDebug: 4.1 ";
-    $stmt->execute();
-    $result = $stmt->get_result();
-    
-echo "updateDebug: 4.2 ";
-    $row = $result->fetch_assoc();
-    if ($row['VALUE'] == 0) {
-        $new_value = 1; // replace with your desired new value
-        $stmt = $conn->prepare("UPDATE drhemo_attempts SET VALUE = ? WHERE SID = ?");
-        $stmt->bind_param("ss", $new_value, $column);
-        $stmt->execute();
-
-        echo "Updated column " . $column . " with value " . $new_value;
-        break;
-    }
-
 echo "updateDebug: 4.3 ";
 
 $results = $column_name->get_result();
     
     
-if ($column_name) {
-  $stmt = $conn->prepare("UPDATE dr_hemo_attempts SET $column_name = ? WHERE SID1 = ? AND SID2 = ? AND SID3 = ? AND SID4 = ? AND SID5 = ?");
-  echo "updateDebug: 6 ";
-  $stmt->bind_param("iiiiii", $sid, $sid1, $sid2, $sid3, $sid4, $sid5);
-  echo "updateDebug: 6.1 ";
-  $stmt->execute();
-}
-echo "updateDebug: 7 ";
-$stmt->bind_param("diii", $time, $completed, $sid, $aid);
+// if ($column_name) {
+//   $stmt = $conn->prepare("UPDATE dr_hemo_attempts SET $column_name = ? WHERE SID1 = ? AND SID2 = ? AND SID3 = ? AND SID4 = ? AND SID5 = ?");
+//   echo "updateDebug: 6 ";
+//   $stmt->bind_param("iiiiii", $sid, $sid1, $sid2, $sid3, $sid4, $sid5);
+//   echo "updateDebug: 6.1 ";
+//   $stmt->execute();
+// }
+//$stmt->bind_param("diii", $time, $completed, $sid, $aid);
+
+
 // return statements
 if ($stmt->execute())
 {
